d8089ed8b368249ed23682125cdc82b9b4937b30
danol0 <116915585+danol0@users.noreply.github.com>
2023-11-22 17:08:31 +0000

    add unit tests for constraints Added tests for the constraint propigation functionality. Now ready to merge with main.

--- test/test_board.py
+++ test/test_board.py
@@ -12,7 +12,10 @@ def test_board_initialization():
 def test_board_printing():
     initial_state = np.ones(81, dtype=int).reshape(9, 9)
     board = SudokuBoard(initial_state)
-    assert str(board) == "111|111|111\n111|111|111\n111|111|111\n---+---+---\n111|111|111\n111|111|111\n111|111|111\n---+---+---\n111|111|111\n111|111|111\n111|111|111\n"
+    assert (
+        str(board)
+        == "111|111|111\n111|111|111\n111|111|111\n---+---+---\n111|111|111\n111|111|111\n111|111|111\n---+---+---\n111|111|111\n111|111|111\n111|111|111\n"
+    )
 
 
 def test_board_invalid_initial_state():
@@ -25,3 +28,24 @@ def test_board_invalid_initial_state():
         SudokuBoard(wrong_type)
     with pytest.raises(ValueError):
         SudokuBoard(double_digit)
+
+
+def test_board_constraint():
+    initial_state = np.array(
+        [
+            [4, 8, 3, 9, 2, 1, 6, 5, 7],
+            [9, 6, 7, 3, 0, 5, 8, 2, 1],
+            [2, 5, 1, 0, 7, 6, 4, 9, 3],
+            [5, 4, 8, 1, 3, 0, 9, 0, 6],
+            [7, 2, 9, 5, 0, 4, 1, 3, 8],
+            [0, 3, 6, 7, 9, 8, 2, 4, 5],
+            [3, 7, 2, 0, 8, 9, 5, 1, 4],
+            [8, 0, 4, 2, 5, 3, 7, 6, 9],
+            [6, 9, 5, 4, 1, 7, 3, 8, 0],
+        ]
+    )
+    board = SudokuBoard(initial_state)
+    assert board.possible_values[3, 7] == {7}
+    assert board.possible_values[8, 8] == {2}
+    assert board.possible_values[0, 0] == {} or {4}  # depends on implimentation
+    assert board.solve()
