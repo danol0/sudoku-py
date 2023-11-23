15a2d5143c1ff51df293b5ba9947fb255eff99c7
danol0 <116915585+danol0@users.noreply.github.com>
2023-11-23 18:15:05 +0000

    add timer main.py now prints the time it took to solve the input board

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 import sys
 import sudoku.tools as st
 from sudoku.board import SudokuBoard
+import time
 
 board = SudokuBoard(st.load_initial_state_to_array(sys.argv[1]))
+start = time.time()
 board.solve()
+end = time.time()
+print("Time taken: ", end - start)
 print(board)
