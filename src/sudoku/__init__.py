2b2de62c42b8137e7bdecc83be6c92d88bab3ea3
danol0 <116915585+danol0@users.noreply.github.com>
2023-12-04 10:33:19 +0000

    add examples to init.py

--- src/sudoku/__init__.py
+++ src/sudoku/__init__.py
@@ -1,10 +1,42 @@
+from . import board
+from . import tools
+
 """
-Sudoku Package
-==============
+sudoku-py
+=========
 
-This package contains modules and functions for solving Sudoku puzzles.
+Python package for solving sudoku puzzles.
 
-Modules:
+Modules
+-------
 - board: Defines the Sudoku board class and its operations.
 - tools: Provides utility functions for loading Sudoku puzzles.
+
+Examples
+--------
+>>> import sudoku
+
+# load the puzzle from file
+>>> state = sudoku.tools.load_initial_state("input.txt")
+
+# initialize board object
+>>> board = sudoku.board.sudokuBoard(state)
+
+# solve the puzzle
+>>> board.solve()
+The puzzle was solved by constraint propagation in 0.0045 seconds.
+
+# display the solved puzzle
+>>> print(board)
+483|921|657
+967|345|821
+251|876|493
+---+---+---
+548|132|976
+729|564|138
+136|798|245
+---+---+---
+372|689|514
+614|253|789
+895|417|362
 """
