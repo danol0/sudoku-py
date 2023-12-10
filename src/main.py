13c6378dbbd0f6ccaba13707ef25e86c00e51f3d
danol0 <116915585+danol0@users.noreply.github.com>
2023-12-10 14:04:14 +0000

    main now runs from config.json Behaviour of command line script can now be modified with config file Options include: - Customising save behaviour - Changing puzzle - Changing solver object parameters Updated dockerfile to copy config Also removed compose.yaml file which was redundant

--- src/main.py
+++ src/main.py
@@ -1,14 +1,43 @@
 from sudoku.solver import sudokuSolver
 import sys
+import json
+import warnings
 
-# check that a puzzle was passed as an argument
-initial_state = sys.argv[1] if len(sys.argv) > 1 else None
-if initial_state is None:
-    raise ValueError("Please specify a puzzle.")
+# check that the config file exists
+try:
+    with open("config.json") as f:
+        config = json.load(f)
+
+except FileNotFoundError:
+    warnings.warn("The config.json file does not exist, using default config.")
+    config = {
+        "initial_state": ".....7........95.4....5.169.8....3.5.75...29.4.6....8.762.8....1.39........6.....",
+        "strategy": "auto",
+        "max_solve_time": 60,
+        "save_path": False,
+    }
+
+# overwrite the config puzzle if user has specified one via the command line
+if len(sys.argv) == 2:
+    initial_state = sys.argv[1]
+    print("Loading puzzle from command line argument")
+elif len(sys.argv) > 2:
+    raise ValueError(
+        "Too many arguments provided. Please provide a single filepath or puzzle string."
+    )
+else:
+    initial_state = config["initial_state"]
+    print("Loading puzzle from config file")
 
 # initialize solver
-board = sudokuSolver(initial_state)
+board = sudokuSolver(
+    initial_state, strategy=config["strategy"], max_solve_time=config["max_solve_time"]
+)
 
 # solve and print the board
 board.solve()
 print(board)
+
+# save the board if a save path is specified
+if config["save_path"]:
+    board.save(config["save_path"])
