ccc3e2c89f858b9371656144f12f79b7b33f0625
danol0 <116915585+danol0@users.noreply.github.com>
2023-12-11 16:41:38 +0000

    improved CLI Main is now much more robust. - Split load config into new function. - Added main function for main script. - Added more informative error messages.

--- src/main.py
+++ src/main.py
@@ -1,43 +1,68 @@
 from sudoku.solver import sudokuSolver
 import sys
 import json
-import warnings
 
-# check that the config file exists
-try:
-    with open("config.json") as f:
-        config = json.load(f)
 
-except FileNotFoundError:
-    warnings.warn("The config.json file does not exist, using default config.")
-    config = {
-        "initial_state": ".....7........95.4....5.169.8....3.5.75...29.4.6....8.762.8....1.39........6.....",
-        "strategy": "auto",
-        "max_solve_time": 60,
-        "save_path": False,
-    }
+def load_config():
+    """
+    Load the configuration from the config.json file.
 
-# overwrite the config puzzle if user has specified one via the command line
-if len(sys.argv) == 2:
-    initial_state = sys.argv[1]
-    print("Loading puzzle from command line argument")
-elif len(sys.argv) > 2:
-    raise ValueError(
-        "Too many arguments provided. Please provide a single filepath or puzzle string."
+    Returns
+    -------
+    config : dict
+        The configuration settings.
+
+    Raises
+    -------
+        FileNotFoundError :
+            If the config.json file is not found.
+    """
+    try:
+        with open("config.json") as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            "Config file not found. Please create a config.json file in the root directory."
+        )
+
+    return config
+
+
+def main():
+    """
+    Main entry point of the script.
+    """
+    # Load the configuration
+    config = load_config()
+
+    # Overwrite the config puzzle if user has specified one via the command line
+    if len(sys.argv) == 2:
+        initial_state = sys.argv[1]
+        print("Loading puzzle from command line argument")
+    elif len(sys.argv) > 2:
+        raise ValueError(
+            "Too many arguments provided. Accepts one argument, the path to the puzzle file, "
+            "or none to load from the config file."
+        )
+    else:
+        initial_state = config["initial_state"]
+        print("Loading puzzle from config file")
+
+    # Initialize the solver
+    board = sudokuSolver(
+        initial_state,
+        strategy=config["strategy"],
+        max_solve_time=config["max_solve_time"],
     )
-else:
-    initial_state = config["initial_state"]
-    print("Loading puzzle from config file")
 
-# initialize solver
-board = sudokuSolver(
-    initial_state, strategy=config["strategy"], max_solve_time=config["max_solve_time"]
-)
+    # Solve and print the board
+    board.solve()
+    print(board)
 
-# solve and print the board
-board.solve()
-print(board)
+    # Save the board if a save path is specified
+    if config["save_path"]:
+        board.save(config["save_path"])
 
-# save the board if a save path is specified
-if config["save_path"]:
-    board.save(config["save_path"])
+
+if __name__ == "__main__":
+    main()
